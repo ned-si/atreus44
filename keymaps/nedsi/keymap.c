7f2ee71e439d51e414141936fe34644e4f6fe695
nedsi <32332697+nedsir@users.noreply.github.com>
2021-06-06 23:43:06 +0200

    refactored to use userspace

--- keymaps/nedsi/keymap.c
+++ keymaps/nedsi/keymap.c
@@ -1,174 +1,66 @@
-#include QMK_KEYBOARD_H
-#include "keymap.h"
-#include "accented_letters.h"
-#include <stdio.h>
-
-// Tap dance states
-typedef enum {
-  SINGLE_TAP,
-  SINGLE_HOLD,
-  DOUBLE_SINGLE_TAP
-} td_state_t;
-
-// create a global instance of the tapdance state type
-static td_state_t td_state;
-
-// function to track the current tapdance state
-int cur_dance (qk_tap_dance_state_t *state);
-
-
-// track the tapdance state to return
-int cur_dance (qk_tap_dance_state_t *state) {
-  if (state->count == 1) {
-    if (state->interrupted || !state->pressed) {
-        return SINGLE_TAP;
-    } else {
-        return SINGLE_HOLD;
-    }
-  }
-  if (state->count == 2) {
-      return DOUBLE_SINGLE_TAP;
-  }
-  else {
-      return 3; // any number higher than the maximum state value you return above
-  }
-};
-
-// handle the possible states for each tapdance keycode you define:
-
-// `finished` function for each tapdance keycode
-void CA_CC_CV_finished (qk_tap_dance_state_t *state, void *user_data);
-
-void CA_CC_CV_finished (qk_tap_dance_state_t *state, void *user_data) {
-  td_state = cur_dance(state);
-  switch (td_state) {
-    case SINGLE_TAP:
-      tap_code16(C(KC_C));
-      break;
-    case SINGLE_HOLD:
-      tap_code16(C(KC_A));
-      break;
-    case DOUBLE_SINGLE_TAP:
-      tap_code16(C(KC_V));
-  }
-};
-
-// `finished` function for each tapdance keycode
-void UNI_W_L_finished (qk_tap_dance_state_t *state, void *user_data);
-
-void UNI_W_L_finished (qk_tap_dance_state_t *state, void *user_data) {
-  td_state = cur_dance(state);
-  switch (td_state) {
-    case SINGLE_TAP:
-      tap_code16(UC_M_WC);
-      break;
-    case DOUBLE_SINGLE_TAP:
-      tap_code16(UC_M_LN);
-    break;
-    default: break;
-  }
-};
-
-// `finished` function for each tapdance keycode
-void NXT_PRV_finished (qk_tap_dance_state_t *state, void *user_data);
-
-void NXT_PRV_finished (qk_tap_dance_state_t *state, void *user_data) {
-  td_state = cur_dance(state);
-  switch (td_state) {
-    case SINGLE_TAP:
-      tap_code16(KC_MNXT);
-      break;
-    case DOUBLE_SINGLE_TAP:
-      tap_code16(KC_MPRV);
-    break;
-    default: break;
-  }
-};
-
-qk_tap_dance_action_t tap_dance_actions[] = {
-    [CA_CC_CV] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, CA_CC_CV_finished, NULL),
-    [UNI_W_L] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, UNI_W_L_finished, NULL),
-    [NXT_PRV] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, NXT_PRV_finished, NULL),
-};
-
-uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case HOME_A:
-            return TAPPING_TERM + 50;
-        case HOME_S:
-            return TAPPING_TERM + 50;
-        /* case HOME_D: */
-        /*     return TAPPING_TERM - 20; */
-        case HOME_F:
-            return TAPPING_TERM - 20;
-        case HOME_G:
-            return TAPPING_TERM - 10;
-        case HOME_H:
-            return TAPPING_TERM - 10;
-        case HOME_J:
-            return TAPPING_TERM - 20;
-        /* case HOME_K: */
-        /*     return TAPPING_TERM - 20; */
-        case HOME_L:
-            return TAPPING_TERM + 50;
-        case HOME_SCLN:
-            return TAPPING_TERM + 50;
-        default:
-            return TAPPING_TERM;
-    }
-};
+#include "nedsi.h"
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_MAIN] = LAYOUT( 
-    KC_Q,    KC_W,    KC_E,    KC_R,      KC_T,                               KC_Y,   KC_U,    KC_I,    KC_O,       KC_P,
-    HOME_A,  HOME_S,  HOME_D,  HOME_F,  HOME_G,                             HOME_H, HOME_J,  HOME_K,  HOME_L,  HOME_SCLN,
-    KC_Z,    KC_X,    KC_C,      KC_V,   KC_B,          KC_CAPS,  KC_DEL,     KC_N,   KC_M, KC_COMM,  KC_DOT,    KC_SLSH,
-    TG(_GAMING), UNIWL,  MO(_SYMBOLS), KC_TAB, CACCCV,     UNDO,    REDO,   KC_ENT, KC_SPC, MO(_NUMBERS), NXTPRV, KC_MPLY
+  [_MAIN] = LAYOUT_atreus44_wrapper( 
+    ________________MAIN_L1________________,                                ________________MAIN_R1________________,
+    ________________MAIN_L2________________,                                ________________MAIN_R2________________,
+    ________________MAIN_L3________________,    KC_CAPS,       KC_DEL,      ________________MAIN_R3________________,
+    TG(_GAMING), UNIWL,  MO(_SYM), KC_TAB, CACCCV,     UNDO,    REDO,       KC_ENT, KC_SPC, MO(_NUM), NXTPRV, KC_MPLY
   ),
 
-  [_GAMING] = LAYOUT(
-    KC_Q,    KC_W,    KC_E,    KC_R,      KC_T,                               KC_Y,   KC_U,    KC_I,    KC_O,       KC_P,
-    HOME_A,  HOME_S,  HOME_D,  HOME_F,  HOME_G,                             HOME_H, HOME_J,  HOME_K,  HOME_L,  HOME_SCLN,
-    KC_Z,    KC_X,    KC_C,      KC_V,   KC_B,      KC_CAPS,  KC_DEL,         KC_N,   KC_M, KC_COMM,  KC_DOT,    KC_SLSH,
-    _______, _______,MO(_SYMBOLS), KC_SPC, KC_TAB,     UNDO,    REDO,       KC_ENT, KC_SPC, MO(_NUMBERS), NXTPRV, KC_MPLY
+  [_GAMING] = LAYOUT_atreus44_wrapper(
+    ________________MAIN_L1________________,                                ________________MAIN_R1________________,
+    ________________GAMING_L2________________,                              ________________GAMING_R2________________,
+    ________________MAIN_L3________________,    KC_CAPS,       KC_DEL,      ________________MAIN_R3________________,
+    _______, _______,MO(_SYM), KC_SPC, KC_TAB,     UNDO,    REDO,       KC_ENT, KC_SPC, MO(_NUM), NXTPRV, KC_MPLY
   ),
 
-  [_SYMBOLS] = LAYOUT(
-    _______, _______, _______,    DEGR,  KC_DQT,                   KC_QUES, KC_EXLM, KC_LCBR, KC_RCBR, _______, 
-    _______, KC_TILD, KC_ASTR,  KC_GRV, KC_QUOT,                    KC_DLR, KC_HASH, KC_LBRC, KC_RBRC, KC_PIPE, 
-    _______, _______, KC_PERC, KC_CIRC, KC_MINS, _______, KC_UNDS, KC_AMPR, KC_PLUS, KC_LPRN, KC_RPRN, KC_BSLS, 
-    _______, _______, _______, _______, _______, _______,   KC_AT,  KC_EQL, _______,MO(_NAV), _______, _______
+  [_SYM] = LAYOUT_atreus44_wrapper(
+    ________________SYM_L1________________,                                ________________SYM_R1________________,
+    ________________SYM_L2________________,                                ________________SYM_R2________________,
+    ________________SYM_L3________________,    KC_CAPS,       KC_DEL,      ________________SYM_R3________________,
+    _______, _______, _______, _______, _______, _______, _______,  KC_EQL, _______,MO(_NAV), _______, _______
   ),
 
-  [_ACCENTS] = LAYOUT( 
-    _______, _______, E_ACUTE, E_GRAVE, _______,                   DED_UML, U_GRAVE, I_CIRCU, O_CIRCU, _______, 
-    A_GRAVE, A_CIRCU, E_CIRCU, _______, _______,                   DED_CIR, _______, _______, _______, _______, 
-    _______, _______, C_CDILA, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+  [_ACC] = LAYOUT_atreus44_wrapper( 
+    ________________ACC_L1________________,                                ________________ACC_R1________________,
+    ________________ACC_L2________________,                                ________________ACC_R2________________,
+    ________________ACC_L3________________,     _______,  _______,         ________________ACC_R3________________,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
 
-  [_NUMBERS] = LAYOUT( 
-      KC_F9,  KC_F10,  KC_F11,  KC_F12, _______,                   _______,    KC_7,    KC_8,    KC_9, _______, 
-      KC_F5,   KC_F6,   KC_F7,   KC_F8, _______,                      KC_0,    KC_4,    KC_5,    KC_6,  KC_DOT, 
-      KC_F1,   KC_F2,   KC_F3,   KC_F4, _______, _______, _______, KC_PPLS,    KC_1,    KC_2,    KC_3,  KC_DOT, 
+  [_NUM] = LAYOUT_atreus44_wrapper( 
+    ________________NUM_L1________________,                                ________________NUM_R1________________,
+    ________________NUM_L2________________,                                ________________NUM_R2________________,
+    ________________NUM_L3________________,     _______,  _______,         ________________NUM_R3________________,
     _______, _______,MO(_NAV), _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
 
-  [_NAV] = LAYOUT( 
-    _______, _______, _______, _______, _______,                   KC_HOME, KC_PGDN, KC_PGUP,  KC_END, _______, 
-    KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,                   KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, 
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+  [_NAV] = LAYOUT_atreus44_wrapper( 
+    ________________NAV_L1________________,                                ________________NAV_R1________________,
+    ________________NAV_L2________________,                                ________________NAV_R2________________,
+    ________________NAV_L3________________,     _______,  _______,         ________________NAV_R3________________,
       RESET, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
-  [_OPTIONS] = LAYOUT( 
-    _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, 
-    _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, 
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+  [_MISC] = LAYOUT_atreus44_wrapper( 
+    ________________NAV_L1________________,                                ________________NAV_R1________________,
+    ________________NAV_L2________________,                                ________________NAV_R2________________,
+    ________________NAV_L3________________,     _______,  _______,         ________________NAV_R3________________,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
   )
 };
+
+__attribute__ ((weak))
+void matrix_init_keymap(void) {}
+void matrix_init_user(void) {
+ 	
+// Atreus 44 is more often on WIN so start in WIN mode
+  set_unicode_input_mode(UC_WINC);
+   
+  matrix_init_keymap();
+}
 
 #ifdef ENCODER_ENABLE
 void encoder_update_user(uint8_t index, bool clockwise) {
--- rules.mk
+++ rules.mk
@@ -1,6 +1,6 @@
 ENCODER_ENABLE = yes       # Enable the use of one or more encoders
 TAP_DANCE_ENABLE = yes     # Activate tap dance
-UNICODEMAP_ENABLE = yes    # Enable unicode map
+
 COMBO_ENABLE = yes 				 # Enable combos
 NKRO_ENABLE = yes 				 # Enable NKRO: any number of key-presses at once
 LTO_ENABLE = yes  				 # Enable Link Time Optimization
